1f7090257662d42b09cb004f5b45e418c0f60786
Sami Hakani <sami.hakani@gmail.com>
2023-09-04 13:56:52 -0400

    Added method to check if game is over

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const Gameboard = (() => { // module pattern for the single game board
     let board = [[0,0,0],[0,0,0],[0,0,0]];
+    let markers = ['X', 'O'];
+    let turnCounter = 0;
 
     const clearBoard = () => {
         Gameboard.board = [[0,0,0],[0,0,0],[0,0,0]];
@@ -13,18 +15,43 @@ const Gameboard = (() => { // module pattern for the single game board
         return Gameboard.board[i][j] === 0;
     };
 
-    const turn = (i, j, marker) => {
+    const turn = (i, j) => {
         if (isValidMove(i, j)) {
+            let marker = Gameboard.markers[Gameboard.turnCounter%2];
             Gameboard.board[i][j] = marker;
             let cell = document.querySelector(`#cell-${i}-${j}`);
             cell.textContent = marker;
+            Gameboard.turnCounter++;
         }
         else {
             return 'INVALID MOVE';
         }
     };
 
-    return {board, clearBoard, isValidMove, turn};
+    const isGameOver = () => { // checks if game is over
+        // first check each row
+        for (let i = 0; i < 3; i++) { // check rows
+            let row = Gameboard.board[i];
+            if (row[0] === row[1] && row[1] == row[2]) {
+                return true;
+            }
+        }
+        for (let j = 0; j < 3; j++) { // check columns
+            if (Gameboard.board[0][j] == Gameboard.board[1][j] && Gameboard.board[1][j] == Gameboard.board[2][j]) {
+                return true;
+            }
+        }
+        // check diagonals
+        if (Gameboard.board[0][0] == Gameboard.board[1][1] == Gameboard.board[2][2]) {
+            return true;
+        }
+        if (Gameboard.board[0][2] == Gameboard.board[1][1] == Gameboard.board[2][0]) {
+            return true;
+        }
+        return false;
+    };
+
+    return {board, markers, turnCounter, clearBoard, isValidMove, turn};
 })();
 
 const Player = (marker) => { // player factory
@@ -41,7 +68,11 @@ for (let i = 0; i < 3; i++) {
         let btn = document.createElement('button');
         btn.classList.add('cell');
         btn.id = `cell-${i}-${j}`;
-        // rowContainer.appendChild(btn);
+
+        btn.addEventListener('click', () => {
+            Gameboard.turn(i, j);
+        });
+
         boardContainer.appendChild(btn);
     }
     // boardContainer.appendChild(rowContainer);
