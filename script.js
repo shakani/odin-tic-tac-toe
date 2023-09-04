7cbd435c145bbe1e356e1b3f979730bb71f3152a
Sami Hakani <sami.hakani@gmail.com>
2023-09-04 13:14:02 -0400

    DOM manipulation to create board

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const Gameboard = (() => { // module pattern for the single game board
 const Player = (marker) => { // player factory
     return {marker};
 }
+
+// DOM manipulation
+
+let boardContainer = document.querySelector('.board');
+for (let i = 0; i < 3; i++) {
+    let rowContainer = document.createElement('div');
+    rowContainer.classList.add('row');
+    for (let j = 0; j < 3; j++) {
+        let btn = document.createElement('button');
+        btn.classList.add('cell');
+        btn.id = `cell-${i}-${j}`;
+        rowContainer.appendChild(btn);
+    }
+    boardContainer.appendChild(rowContainer);
+}
